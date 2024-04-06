267782d71eb87f15f2e454ba54c780b1ac37dc4a
Ashish Khare <akstyles211@gmail.com>
2024-04-06 10:44:33 +0530

    Replaced 3D with video

--- components/landing/Artwork.jsx
+++ components/landing/Artwork.jsx
@@ -1,14 +1,10 @@
-"use client";
-
-import Spline from "@splinetool/react-spline";
-
 export default function (props) {
 	return (
 		<div className="absolute top-0 left-1/2 w-full h-full">
-			<Spline
-				scene="https://prod.spline.design/AXxqcbVKyKraHjNS/scene.splinecode"
-				className="h-full -translate-y-1/2"
-			/>
+			<video autoPlay muted loop className="w-full h-full object-cover">
+	            <source src="/output.mp4" type="video/mp4" />
+	            Your browser does not support the video tag.
+	        </video>
 		</div>
 	);
 }
