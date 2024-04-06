72113dccd0d001fdd366ed45610ffab7f89f4af1
Ashish Khare <akstyles211@gmail.com>
2024-04-06 10:52:35 +0530

    Fixed phone screen size bug

--- components/landing/Artwork.jsx
+++ components/landing/Artwork.jsx
@@ -1,7 +1,7 @@
 export default function (props) {
 	return (
-		<div className="absolute top-0 left-1/2 w-full h-full">
-			<video autoPlay muted loop className="w-full h-full object-cover">
+		<div className="absolute inset-y-0 translate-y-1/4 sm:-translate-y-1/2 left-1/2 w-full h-full">
+			<video autoPlay muted loop className="w-full object-cover">
 	            <source src="/output.mp4" type="video/mp4" />
 	            Your browser does not support the video tag.
 	        </video>
